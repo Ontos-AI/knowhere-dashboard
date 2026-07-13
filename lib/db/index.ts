39e0ea9d57f6ece2e6cfc1f3f2cc7584ab505557
suguanYang <wangbinqi77@gmail.com>
2026-07-13 12:28:46 +0800

    Move newsletter storage to dedicated database

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,28 +1,9 @@
 import * as authSchema from "@lib/db/auth-schema";
+import { getDatabaseSslConfig } from "@lib/db/database-ssl";
 import * as appSchema from "@lib/db/schema";
 import { env } from "@lib/env";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-
-const UNSAFE_DB_SSL_ENABLED_VALUES = new Set(["1", "true", "yes", "on"]);
-
-function getDatabaseSslConfig(unsafeDbSslEnabled: string | undefined):
-  | false
-  | {
-      rejectUnauthorized: boolean;
-    } {
-  const shouldDisableDatabaseSsl = UNSAFE_DB_SSL_ENABLED_VALUES.has(
-    (unsafeDbSslEnabled ?? "false").trim().toLowerCase()
-  );
-
-  if (shouldDisableDatabaseSsl) {
-    return false;
-  }
-
-  return {
-    rejectUnauthorized: true,
-  };
-}
 
 // Connection pool for dashboard auth/account data.
 const pool = new Pool({
